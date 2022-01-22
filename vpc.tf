30e18c2d47a2d74e48e69740f1df1e584ec4b6e7
Zico Deng <zicodeng@gmail.com>
2022-01-22 13:00:36 -0800

    Remove redundant tag quote

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "transfer_family_vpc" {
   cidr_block = "10.0.0.0/24"
   tags = {
-    "Name" = "transfer-family-vpc"
+    Name = "transfer-family-vpc"
   }
 }
 
@@ -19,7 +19,7 @@ resource "aws_default_security_group" "transfer_family_sg" {
 resource "aws_internet_gateway" "transfer_family_internet_gateway" {
   vpc_id = aws_vpc.transfer_family_vpc.id
   tags = {
-    "Name" = "transfer-family-internet-gateway"
+    Name = "transfer-family-internet-gateway"
   }
 }
 
@@ -34,7 +34,7 @@ resource "aws_subnet" "transfer_family_subnet_1" {
   cidr_block        = "10.0.0.0/25"
   availability_zone = local.az_1
   tags = {
-    "Name" = "transfer-family-subnet-1"
+    Name = "transfer-family-subnet-1"
   }
 }
 
@@ -43,7 +43,7 @@ resource "aws_subnet" "transfer_family_subnet_2" {
   cidr_block        = "10.0.0.128/25"
   availability_zone = local.az_2
   tags = {
-    "Name" = "transfer-family-subnet-2"
+    Name = "transfer-family-subnet-2"
   }
 }
 
